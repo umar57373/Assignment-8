4ba58115c2cfa43e458f3860cc51ba98235efc9b
umar57373 <shaikv0111@gmail.com>
2024-11-08 13:38:50 +0530

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
 					<>
 						<div className="greet">
 							<p>
-								<span>Hello , Sohil </span>
+								<span>Hello , Team</span>
 							</p>
 							<p>How Can i Help You Today?</p>
 						</div>
